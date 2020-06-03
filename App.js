837ae2edf9ee37f8c1d1528b246762d0d792de38
Tomas <tgargiulo4@gmail.com>
2020-06-03 17:15:17 -0300

    match found on every call fix

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import TicTacToeApp from "./components/TicTacToeApp";
 import io from "socket.io-client";
 
 const socketRoute = "https://stormy-mesa-81778.herokuapp.com/";
+//const socketRoute = "http://localhost:3000/";
 const socket = io(socketRoute);
 
 export default class App extends React.Component {
