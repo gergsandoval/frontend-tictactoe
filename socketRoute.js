3201a11b7810b30038bbd611d6408556eced924a
Sandoval German <GS25398@GSCorp.AD>
2020-06-09 15:06:36 -0300

    another heroku repo

--- socketRoute.js
+++ socketRoute.js
@@ -1,4 +1,5 @@
 const herokuSocketRoute = "https://fast-woodland-88524.herokuapp.com/"; //
 //const herokuSocketRoute = "http://localhost:3000/";
+//const herokuSocketRoute = "https://intense-ocean-36715.herokuapp.com/";
 
 export { herokuSocketRoute };
