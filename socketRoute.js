45ce076b0324b393461e647004ea49677b2acfd0
Sandoval German <GS25398@GSCorp.AD>
2020-06-11 13:25:59 -0300

    change route

--- socketRoute.js
+++ socketRoute.js
@@ -1,4 +1,4 @@
-const herokuSocketRoute = "https://fast-woodland-88524.herokuapp.com/"; // master
-//const herokuSocketRoute = "https://intense-ocean-36715.herokuapp.com/"; // germany
+//const herokuSocketRoute = "https://fast-woodland-88524.herokuapp.com/"; // master
+const herokuSocketRoute = "https://intense-ocean-36715.herokuapp.com/"; // germany
 //const herokuSocketRoute = "http://localhost:3000/";
 export { herokuSocketRoute };
