711d485f5e088aaa60e4f1fbb35b402de3bcd872
Tomas <tgargiulo4@gmail.com>
2020-06-16 22:34:07 -0300

    remove import

--- components/Game/Board.js
+++ components/Game/Board.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, ImageComponent } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import Square from "./Square";
 import GameOverPopUp from "./GameOverPopUp";
 import SocketContext from "../../socket-context";
